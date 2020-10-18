b8651075c91e9e40c03a7ee3535effaaad7eeb2c
baiwensimi <baiwensimi@gmail.com>
2020-10-18 21:48:16 +0800

    v1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='arithm4child',
-    version='1.1',
+    version='1.2',
     packages=['arithm4child'],
     author="Bai Wensimi",
     author_email="baiwensimi@gmail.com",
